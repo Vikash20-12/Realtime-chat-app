e25c00821e7fbc96f52884b1bab87a173200055f
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-06-27 15:48:38 +0530

    third commit

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ io.on('connection', (socket)=>{
     //Broadcast when a user connects
     socket.broadcast.to(user.room).emit('message',formatMessage(botName,`${user.username} has joined the chat`));
 
+    //Send users and room info
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    });
+
     });
 
     //listen for chatMessage
@@ -42,6 +48,12 @@ io.on('connection', (socket)=>{
         const user = userLeave(socket.id);
         if(user){
             io.to(user.room).emit('message',formatMessage(botName,`${user.username} has left the chat`));
+
+            //Send users and room info
+            io.to(user.room).emit('roomUsers',{
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
         }
 
     });
